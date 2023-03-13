d674c60c44d5d7f1c0b1c43096230d18ed71f86e
Rajan <rajaninspirion@gmail.com>
2023-03-13 10:16:06 +0545

    "using select"

--- functions/helper.go
+++ functions/helper.go
@@ -22,12 +22,12 @@ func FanIn(input1, input2 <-chan string) <-chan string {
 	c := make(chan string)
 	go func() {
 		for {
-			c <- <-input1
-		}
-	}()
-	go func() {
-		for {
-			c <- <-input2
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
 		}
 	}()
 	return c
